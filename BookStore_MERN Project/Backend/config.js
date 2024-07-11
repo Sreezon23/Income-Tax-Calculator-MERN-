7ca2acbde288e85f08c83f98d954b0b332415ae3
Sreezon23 <158078280+Sreezon23@users.noreply.github.com>
2024-07-11 19:08:21 +0600

    Delete BookStore_MERN Project directory

--- BookStore_MERN Project/Backend/config.js
+++
@@ -1,3 +0,0 @@
-export const PORT = 5555;
-
-export const mongoDBURL = 'mongodb+srv://sreezon23:oC8IAfMrVCAsaB12@books-store-mern.jdxvsqw.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Books-Store-MERN'
